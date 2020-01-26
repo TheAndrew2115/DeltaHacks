7354a013ccd94ef0c816d52e3504170a0966db65
Andrew <324.andrew.wang@gmail.com>
2020-01-26 00:50:35 -0500

    Added more content to about.html

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
 
 app = Flask(__name__)
 
@@ -6,9 +7,17 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/ape")
-def ape():
+@app.route("/scanner")
+def scanner():
     return render_template("about.html")
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      print("success")
+      return 'file uploaded successfully'
 
 if __name__ == "__main__":
     app.run(debug=True)
